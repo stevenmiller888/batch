618f73b59c20c49e3dca533bf6a39b4559793ae4
stevenmiller888 <stevenmiller888@me.com>
2015-03-27 17:47:01 -0700

    updated test/index.js

--- test/index.js
+++ test/index.js
@@ -28,4 +28,37 @@ describe('batch', function(){
         done();
       });
   });
+
+  it('should pass back the results in order', function(done){
+    batch
+      .push(function(done){
+        setTimeout(function(){
+          done(null, 'foo');
+        }, 500);
+      })
+      .push(function(done){
+        setTimeout(function(){
+          done(null, 'bar');
+        }, 100);
+      })
+      .end(function(err, res){
+        if (err) return done(err);
+        res.should.eql(['foo', 'bar']);
+        done();
+      });
+  });
+
+  it('should only pass the first error back', function(done){
+    batch
+      .push(function(done){
+        done('Error1', 'foo');
+      })
+      .push(function(done){
+        done('Error2', 'bar');
+      })
+      .end(function(err, res){
+        err.should.eql('Error1');
+        done();
+      });
+  });
 });
